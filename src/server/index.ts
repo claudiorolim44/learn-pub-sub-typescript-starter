88212b15e63b967cb6c1fafc5d047fbbfea1cf23
Claudio Augusto Pereira Rolim <claudioaugusto44@gmail.com>
2026-05-19 18:50:20 -0300

    feat(server): publish game state to RabbitMQ

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,43 @@
+import amqp from 'amqplib'
+import { publishJSON } from '../internal/pubsub/publish.js'
+import { ExchangePerilDirect, PauseKey } from '../internal/routing/routing.js'
+import type { PlayingState } from '../internal/gamelogic/gamestate.js'
+
 async function main() {
-  console.log("Starting Peril server...");
+  const rabbitConnString = 'amqp://guest:guest@localhost:5672/'
+  const conn = await amqp.connect(rabbitConnString)
+
+  console.log('Peril game server connected to RabbitMQ!')
+  ;['SIGINT', 'SIGTERM'].forEach((signal) =>
+    process.on(signal, async () => {
+      try {
+        await conn.close()
+        console.log('RabbitMQ connection closed.')
+      } catch (err) {
+        console.error('Error closing RabbitMQ connection:', err)
+      } finally {
+        process.exit(0)
+      }
+    }),
+  )
+
+  const publishChannel = await conn.createConfirmChannel()
+
+  const publishValue: PlayingState = { isPaused: true }
+
+  try {
+    await publishJSON<PlayingState>(
+      publishChannel,
+      ExchangePerilDirect,
+      PauseKey,
+      publishValue,
+    )
+  } catch (err) {
+    console.error('Error publishing message:', err)
+  }
 }
 
 main().catch((err) => {
-  console.error("Fatal error:", err);
-  process.exit(1);
-});
+  console.error('Fatal error:', err)
+  process.exit(1)
+})
